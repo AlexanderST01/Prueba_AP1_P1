841eb7c8b332924affaab413a5516bc51dbed462
AlexanderST01 <137325892+AlexanderST01@users.noreply.github.com>
2023-09-29 13:10:34 -0400

    Agregando modelo

---
+++ Models/Ingresos.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Prueba_AP1_P1.Models
+{
+    public class Ingresos
+    {
+        [Key]
+        public int IngresosId { get; set; }
+        [Required (ErrorMessage ="El campo fecha es requirido")]
+        public DateTime Fecha { get; set; } = DateTime.Now;
+        [Required(ErrorMessage = "El campo concepto es requirido")]
+        [StringLength(100, ErrorMessage ="El campo concepto no debe pasar de 100 caracteres")]
+        public string? Concepto { get; set; }
+        [Required(ErrorMessage ="El compo monto es requierido")]
+        public decimal Monto { get; set; }
+    }
+}
