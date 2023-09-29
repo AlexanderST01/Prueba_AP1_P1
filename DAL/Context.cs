bf6853482464468289d707d4e246b7347891f0ca
AlexanderST01 <137325892+AlexanderST01@users.noreply.github.com>
2023-09-29 13:10:46 -0400

    Agregando contexto

---
+++ DAL/Context.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Prueba_AP1_P1.Models;
+
+namespace Prueba_AP1_P1.DAL
+{
+    public class Context: DbContext
+    {
+        public DbSet<Ingresos> Ingresos { get; set; }
+        public Context(DbContextOptions<Context> options): base(options) { }
+    }
+}
